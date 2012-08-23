b246952b21e6ff5df0ce2a150249b7c56d3a965d
Rob <robin.bygrave@gmail.com>
2012-08-24 00:47:46 +1200

    And add LoadStatistics

---
+++ src/main/java/org/avaje/metric/LoadStatistics.java
@@ -0,0 +1,28 @@
+package org.avaje.metric;
+
+/**
+ * Counter statistics.
+ */
+public interface LoadStatistics extends Statistics {
+
+  /**
+   * Return the time these statistics started being collected.
+   */
+  public long getStartTime();
+  
+  /**
+   * Return the duration in seconds.
+   */
+  public long getDuration();
+
+  /**
+   * Return the count of events.
+   */
+  public long getCount();
+
+  /**
+   * Return the total load value.
+   */
+  public long getLoad();
+  
+}
