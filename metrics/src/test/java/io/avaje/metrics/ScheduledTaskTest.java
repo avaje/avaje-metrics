4a4ea61dd92704ad7d9b83b77dd34d88bd4de628
Rob Bygrave <robin.bygrave@gmail.com>
2024-06-12 22:21:29 +1200

    Add ScheduledTask to help execute periodic reporting of metrics

---
+++ metrics/src/test/java/io/avaje/metrics/ScheduledTaskTest.java
@@ -0,0 +1,44 @@
+package io.avaje.metrics;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ScheduledTaskTest {
+
+  @Test
+  void runIt() throws InterruptedException {
+
+    ScheduledTask task = ScheduledTask.builder()
+      .schedule(1, 1, TimeUnit.MILLISECONDS)
+      .task(ScheduledTaskTest::hello)
+      .build();
+
+    assertThat(counter.get()).isEqualTo(0);
+    System.out.println("start...");
+    task.start();
+    Thread.sleep(5);
+    assertThat(counter.get()).isGreaterThan(1);
+
+    task.waitIfRunning(10, TimeUnit.SECONDS);
+    Thread.sleep(30);
+
+    System.out.println("cancel...");
+    task.cancel(false);
+    long after0 = counter.get();
+    assertThat(after0).isGreaterThan(1);
+    Thread.sleep(10);
+    long after1 = counter.get();
+    assertThat(after1).isEqualTo(after0);
+    System.out.println("done");
+  }
+
+  private static final AtomicLong counter = new AtomicLong();
+
+  private static void hello() {
+    System.out.println("hi " + counter.incrementAndGet());
+  }
+}
